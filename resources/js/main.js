d8439974c874a605532c04fa143fa24cff364d7c
nirajx1d <32599550+nirajx1d@users.noreply.github.com>
2017-12-19 22:31:53 +0530

    Changes in the js file and few other small changes
    The backgound of index.html is now white, some js file now includes some animations and fading effects in the main page

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
 
-  /*$('#slider').hover(
-    function(){
-    $('#slider').animate({top:'+=10px'},500);
-  },
-  function(){
-    $('#slider').animate({top:'-=10px'},500);
-  }
-  );*/
-
+  $('#main-text').hide().delay(200).fadeIn('slow');
+  $('.ieee-logo').hide().delay(200).fadeIn('slow');
+  $('#scroll-down-icon').hide().delay(200).fadeIn('slow');
+  
   //Below code block is for smooth scrolling on clicking of internal links 
+  
   $('#slider').click(function(e){
     e.preventDefault();  //To prevent the default action on clicking the internal links
     var linkHref = $(this).attr('href');  
@@ -18,15 +14,15 @@ $(document).ready(function(){
     },700);       //700 milliseconds...
   });
 
-  //Hover effect on the ieee logo(main page)
   $('.ieee-logo').hover(
     function(){
-      $(this).addClass('ieee-logo-hover');
-    },
-    function(){
-      $(this).removeClass('ieee-logo-hover');
+      $(this).stop().animate({height: '+=4px'},100);
+      $(this).stop().animate({width: '+=6.83px'},100);
+    }, function(){
+      $(this).stop().animate({height: '-=4px'},100);
+      $(this).stop().animate({width: '-=6.83px'},100);
     }
-    );
-
+  );
+    
 
 });
