dee2bae0f3c66767fe10ee4f9f4da6186012051e
Niraj Shubham <32599550+nirajx1d@users.noreply.github.com>
2018-03-15 21:10:37 +0530

    added popup.js

---
+++ resources/js/popup.js
@@ -0,0 +1,12 @@
+
+function close_popup(){
+    $('.popup').toggle();
+    $('.ieee-logo').toggle();
+    $('#main-text').toggle();
+}
+
+function open_popup(){
+    $('.popup').css("display","block");
+    $('.ieee-logo').toggle();
+    $('#main-text').toggle();
+}
